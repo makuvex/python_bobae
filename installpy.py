c734d621bc0f398e1e67cb775a570134536533ad
makuvex <makuvex7@gmail.com>
2020-01-27 15:33:03 +0000

    update

--- installpy.py
+++ installpy.py
@@ -1,1 +1,9 @@
-pip3 install flask-restful
+apt-get install python-requests
+pip install --upgrade pip
+pip install requests
+pip install flask-restful
+pip install urlopen
+pip install google_images_download
+
+#파이썬 한글
+# -*- coding: utf-8 -*- 
